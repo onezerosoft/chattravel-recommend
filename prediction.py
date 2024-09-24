f58f74187d0d624d14921d7f09b89b449dd86e7b
yunju <juile0827@naver.com>
2024-09-24 15:45:24 +0900

    추천 모델 결과, json 반환

--- prediction.py
+++ prediction.py
@@ -2,6 +2,8 @@ import pickle
 import pandas as pd
 import os
 import sys
+import re
+import json
 
 def load_model(region):
     base_path = 'model'
@@ -32,29 +34,13 @@ def load_model(region):
     return place_model, accommodation_model
 
 
-def load_data(region, si):
+def load_data(si):
     
-    if region == 'E':  # 수도권
-        df1 = pd.read_csv('data/preprocessed/여행지/dfE.csv')
-        df2 = pd.read_csv('data/preprocessed/숙박/dfE.csv')
-    
-    elif region == 'F':  # 동부권
-        df1 = pd.read_csv('data/preprocessed/여행지/dfF.csv')
-        df2 = pd.read_csv('data/preprocessed/숙박/dfF.csv')
-    
-    elif region == 'G':  # 서부권
-        df1 = pd.read_csv('data/preprocessed/여행지/dfG.csv')
-        df2 = pd.read_csv('data/preprocessed/숙박/dfG.csv')
-    
-    elif region == 'H':  # 제주
-        df1 = pd.read_csv('data/preprocessed/여행지/dfH.csv')
-        df2 = pd.read_csv('data/preprocessed/숙박/dfH.csv')
-    
-    else:
-        raise ValueError("Invalid region code. Please use 'E', 'F', 'G', or 'H'.")
-    
-    df1 = df1[df1['SI']==si]
-    df2 = df2[df2['SI']==si]
+    df1 = pd.read_csv('data/preprocessed/여행지/df_total.csv')
+    df2 = pd.read_csv('data/preprocessed/숙박/df_total.csv')
+        
+    df1 = df1[df1['SI'].isin(si)]
+    df2 = df2[df2['SI'].isin(si)]
     
     return df1, df2
 
@@ -74,16 +60,15 @@ def predict_place(model, df, user_id, num):
     # 예측된 점수로 정렬하여 상위 N개의 아이템 추천
     top_n_predictions = sorted(predictions, key=lambda x: x.est, reverse=True)[:num]
 
-    # 추천 결과 출력
-    
-    print("추천 여행지")
+    # 추천 결과 
+    result = []
     for prediction in top_n_predictions:
-        #print(prediction)
-        print(f"Item: {prediction.iid}, Predicted Rating: {prediction.est}")
+        result.append({
+           "Item": prediction.iid,
+           "PredictedRating": prediction.est
+        })
     
-    print(" ")
-    
-    return top_n_predictions
+    return result
 
 
 def predict_accommodation(model, df, user_id, num):
@@ -100,38 +85,65 @@ def predict_accommodation(model, df, user_id, num):
     # 예측된 점수로 정렬하여 상위 N개의 아이템 추천
     top_n_predictions = sorted(predictions, key=lambda x: x.est, reverse=True)[:num]
 
-    # 추천 결과 출력
-    print("추천 숙박지")
+  
+    # 추천 결과 
+    result = []
     for prediction in top_n_predictions:
-        #print(prediction)
-        print(f"Item: {prediction.iid}, Predicted Rating: {prediction.est}")
-    print(" ")
+        result.append({
+           "Item": prediction.iid,
+           "PredictedRating": prediction.est
+        })
     
-    return top_n_predictions
+    return result
 
 
 def main():
 
-    region = sys.argv[1]
-    si = sys.argv[2] # 여행지 시(군)
-    place_num = int(sys.argv[3]) # 추천 여행지 갯수
-    accom_num = int(sys.argv[4]) # 추천 숙박지 갯수
+    user_id = sys.argv[1] # 사용자 아이디
+    sido = sys.argv[2] # 여행지 시도
+    si = re.split(r',\s*', sys.argv[3].strip('[]')) # 여행지 시(군) 리스트
+    day = int(sys.argv[4]) # 여행일수
 
-  
+    # 여행지 / 숙박지 추천 갯수
+    if day == 1:  #당일치기
+      place_num = 3 
+      accom_num = 0 
+    else:
+      place_num = day * 3 - 2
+      accom_num = 1     
+
+    # region
+    if sido in ["서울", "경기도", "강원도"]:
+      region = 'E'
+
+    elif sido in ["경상북도", "경상남도"]:
+      region = 'F'
+
+    elif sido in ["충청북도", "충청남도", "전라북도", "전라남도"]:
+      region = 'G'
+    
+    elif sido in ["제주도"]:
+      region = 'H'
+    
+
     # 지역별 추천 모델과 데이터 불러오기
     place_model, accommodation_model = load_model(region)
-    df1 , df2 = load_data(region, si)
+    df1 , df2 = load_data(si)
 
-    # 사용자 아이디
-    user_id = 'h000617'
     
     # 여행지 추천 모델
     place_predictions = predict_place(place_model, df1, user_id, place_num)
 
     # 숙박 장소 추천 모델
-    user_id = 'h_h000617'
     accommodation_predictions = predict_accommodation(accommodation_model, df2, user_id, accom_num)
 
-    
+    result = {
+       "place": place_predictions,
+       "accommodation": accommodation_predictions
+    }
+
+    print(json.dump(result, ensure_ascii=False))
+
 if __name__ == "__main__":
     main()
+  
