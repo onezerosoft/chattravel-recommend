7c09717350f608cc86233d75bcd2535e7ca4895d
yunju <juile0827@naver.com>
2024-09-28 00:17:25 +0900

    데이터 SIDO, SI 정돈

--- src/data/SI_processing.py
+++ src/data/SI_processing.py
@@ -1,34 +1,48 @@
 import pandas as pd
+import os
 
 # CSV 파일 불러오기
-#df = pd.read_csv('data\preprocessed\여행지\df_total.csv')
-df = pd.read_csv('data\preprocessed\숙박\df_total.csv')
+current_dir = os.path.dirname(os.path.abspath(__file__))
+print("현재 스크립트가 실행 중인 디렉토리:", current_dir)
+
+# 상대 경로를 사용하여 파일 경로 구성
+#path = os.path.join(current_dir, 'preprocessed', 'place', 'df_total.csv')
+path = os.path.join(current_dir, 'preprocessed', 'accomodation', 'df_total.csv')
+
+df = pd.read_csv(path)
 
 # SI 변경
-df.loc[df['SIDO'] == '세종특별시', 'SI'] = '세종특별시'
-df.loc[df['SIDO'].isin(['부산', '부산광역시']), 'SI'] = '부산광역시'
-df.loc[df['SIDO'].isin(['울산', '울산광역시']), 'SI'] = '울산광역시'
-df.loc[df['SIDO'].isin(['대구', '대구광역시']), 'SI'] = '대구광역시'
-df.loc[df['SIDO'].isin(['광주', '광주광역시']), 'SI'] = '광주광역시'
-df.loc[df['SIDO'].isin(['대전', '대전광역시']), 'SI'] = '대전광역시'
+df.loc[df['SIDO'] == '서울', 'SI'] = '서울시'
+# df.loc[df['SIDO'].isin(['부산', '부산광역시']), 'SI'] = '부산광역시'
+# df.loc[df['SIDO'].isin(['울산', '울산광역시']), 'SI'] = '울산광역시'
+# df.loc[df['SIDO'].isin(['대구', '대구광역시']), 'SI'] = '대구광역시'
+# df.loc[df['SIDO'].isin(['광주', '광주광역시']), 'SI'] = '광주광역시'
+# df.loc[df['SIDO'].isin(['대전', '대전광역시']), 'SI'] = '대전광역시'
 
 
 # 'SI' 컬럼에서 '1'을 '2'로 변경
-df['SI'] = df['SI'].replace('부산', '부산광역시')
-df['SI'] = df['SI'].replace('울산', '울산광역시')
-df['SI'] = df['SI'].replace('대구', '대구광역시')
-df['SI'] = df['SI'].replace('광주', '광주광역시')
-df['SI'] = df['SI'].replace('대전', '대전광역시')
+# df['SI'] = df['SI'].replace('부산', '부산광역시')
+# df['SI'] = df['SI'].replace('울산', '울산광역시')
+# df['SI'] = df['SI'].replace('대구', '대구광역시')
+# df['SI'] = df['SI'].replace('광주', '광주광역시')
+# df['SI'] = df['SI'].replace('대전', '대전광역시')
 
-df['SIDO'] = df['SIDO'].replace('부산', '부산광역시')
-df['SIDO'] = df['SIDO'].replace('울산', '울산광역시')
-df['SIDO'] = df['SIDO'].replace('대구', '대구광역시')
-df['SIDO'] = df['SIDO'].replace('광주', '광주광역시')
-df['SIDO'] = df['SIDO'].replace('대전', '대전광역시')
+df['SIDO'] = df['SIDO'].replace('경기', '경기도')
+df['SIDO'] = df['SIDO'].replace('강원', '강원도')
+df['SIDO'] = df['SIDO'].replace('경북', '경상북도')
+df['SIDO'] = df['SIDO'].replace('경남', '경상남도')
+df['SIDO'] = df['SIDO'].replace('전북', '전라북도')
+df['SIDO'] = df['SIDO'].replace('충북', '충청북도')
+df['SIDO'] = df['SIDO'].replace('충남', '충청남도')
+df['SIDO'] = df['SIDO'].replace('제주특별자치도', '제주도')
+
 
 # 수정된 CSV 파일 저장
-#df.to_csv('data\preprocessed\여행지\df_total.csv', index=False)
-df.to_csv('data\preprocessed\숙박\df_total.csv', index=False)
-#df.to_csv('data\preprocessed\df_test.csv', index=False)
+#path = os.path.join(current_dir, 'preprocessed', 'place', 'df_total.csv')
+#path = os.path.join(current_dir, 'preprocessed', 'accomodation', 'df_total.csv')
+path = os.path.join(current_dir, 'preprocessed', 'df_test.csv')
+
+print(path)
+df.to_csv(path, index=False)
 
 print("CSV 파일이 성공적으로 수정되었습니다.")
